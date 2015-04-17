e72c4440f9d6b070ff759e1095171d5afed95af8
Andrew Chin <chin@juniper.net>
2015-04-16 23:27:05 -0500

    Initial token parsing.

--- parser.py
+++ parser.py
@@ -8,27 +8,46 @@ class Message(object):
         self.data = None
 
     def parse(self):
+        tokens = self.message.split()
+        for token in tokens:
+            if token.startswith('@'):
+                self.mention(token)
+            elif token.startswith('(') and token.endswith(')'):
+                self.emoticon(token)
+            elif self.url(token) == True:
+                self.link(token)
+
         return json.dumps(self.data, sort_keys=True, indent=2)
 
     def mention(self, user):
-        pass
+        print('mention: {}'.format(user))
 
     def emoticon(self, emoticon):
-        pass
+        print('emoticon: {}'.format(emoticon))
 
     def link(self, url):
-        pass
+        print('url: {}'.format(url))
+
+    def url(self, token):
+        """Check to see if a token is a url."""
+        url_prefixes = ['http://', 'https://', 'ftp://', 'ssh://']
+        for prefix in url_prefixes:
+            if token.startswith(prefix):
+                return True
+        return False
 
 
 def main():
+    test_input = '@foo hello world (allthethings) bar https://example.com bye! asdf@asdf.com'
+
     while True:
         input = raw_input('>')
-        if input:
-            message = Message(input)
-            output = message.parse()
-            print(output)
-        else:
-            break
+        if not input:
+            input = test_input
+        message = Message(input)
+        output = message.parse()
+        print(output)
+
 
 if __name__ == '__main__':
     main()

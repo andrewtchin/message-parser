5f967eacb8c141288fd4ac7d51f5b3fe41238952
Andrew Chin <chin@juniper.net>
2015-04-16 22:48:30 -0500

    Initial functions.

--- parser.py
+++ parser.py
@@ -1,14 +1,32 @@
+import json
+
+
 class Message(object):
 
-    def __init__(self, data):
-        self.data = data
+    def __init__(self, message):
+        self.message = message
+        self.data = None
+
+    def parse(self):
+        return json.dumps(self.data, sort_keys=True, indent=2)
+
+    def mention(self, user):
+        pass
+
+    def emoticon(self, emoticon):
+        pass
+
+    def link(self, url):
+        pass
 
 
 def main():
     while True:
-        data = raw_input('>')
-        if data:
-            print data
+        input = raw_input('>')
+        if input:
+            message = Message(input)
+            output = message.parse()
+            print(output)
         else:
             break
 

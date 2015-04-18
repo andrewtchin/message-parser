5640d6499111f9b8782659081bd898f6adf74a99
Andrew Chin <chin@juniper.net>
2015-04-17 22:23:41 -0500

    Initial population of dict with parsed data.

--- parser.py
+++ parser.py
@@ -5,7 +5,7 @@ class Message(object):
 
     def __init__(self, message):
         self.message = message
-        self.data = None
+        self.data = {}
 
     def parse(self):
         tokens = self.message.split()
@@ -20,25 +20,38 @@ class Message(object):
         return json.dumps(self.data, sort_keys=True, indent=2)
 
     def mention(self, user):
-        print('mention: {}'.format(user))
+        if not self.data.get('mentions'):
+            self.data['mentions'] = list()
+        self.data['mentions'].append(user.lstrip('@'))
 
     def emoticon(self, emoticon):
-        print('emoticon: {}'.format(emoticon))
+        if not self.data.get('emoticons'):
+            self.data['emoticons'] = list()
+        self.data['emoticons'].append(emoticon.strip('()'))
 
     def link(self, url):
-        print('url: {}'.format(url))
+        if not self.data.get('links'):
+            self.data['links'] = list()
+        title = self.get_title(url)
+        self.data['links'].append({'url': url, "title": title})
 
     def url(self, token):
-        """Check to see if a token is a url."""
+        """Check to see if a token is a url.
+        TODO: check for email addresses
+        """
         url_prefixes = ['http://', 'https://', 'ftp://', 'ssh://']
         for prefix in url_prefixes:
             if token.startswith(prefix):
                 return True
         return False
 
+    def get_title(self, url):
+        return None
+
 
 def main():
-    test_input = '@foo hello world (allthethings) bar https://example.com bye! asdf@asdf.com'
+    test_input = '@foo hello world (allthethings) @bar https://example.com\
+                  (notbad) ftp://filez.com bye! asdf@asdf.com'
 
     while True:
         input = raw_input('>')

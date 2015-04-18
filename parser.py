9ce9489ca083dfc2301f738611eb409e7a484400
Andrew Chin <chin@juniper.net>
2015-04-17 23:43:14 -0500

    Clean up copypasta code.

--- parser.py
+++ parser.py
@@ -2,6 +2,15 @@ import json
 import re
 
 
+def ensure_key(func):
+    def wrapper(*args):
+        key = args[1]
+        if not args[0].data.get(key):
+            args[0].data[key] = list()
+        return func(*args)
+    return wrapper
+
+
 class Message(object):
 
     def __init__(self, message):
@@ -9,37 +18,34 @@ class Message(object):
         self.data = {}
 
     def parse(self):
-        """ TODO: pretty"""
         tokens = self.message.split()
         for token in tokens:
             if token.startswith('@'):
-                self.add_mention(token)
+                self.add_attribute('mentions', self.get_mention(token))
             elif token.startswith('(') and token.endswith(')'):
-                self.add_emoticon(token)
-            elif self.url(token):
-                self.add_link(token)
-            elif self.email(token):
-                self.add_email(token)
+                self.add_attribute('emoticons', self.get_emoticon(token))
+            elif self.check_url(token):
+                self.add_attribute('links', self.get_link(token))
+            elif self.check_email(token):
+                self.add_attribute('emails', token)
 
         return json.dumps(self.data, sort_keys=True, indent=2)
 
-    def add_mention(self, user):
-        if not self.data.get('mentions'):
-            self.data['mentions'] = list()
-        self.data['mentions'].append(user.lstrip('@'))
+    @ensure_key
+    def add_attribute(self, key, value):
+        self.data[key].append(value)
 
-    def add_emoticon(self, emoticon):
-        if not self.data.get('emoticons'):
-            self.data['emoticons'] = list()
-        self.data['emoticons'].append(emoticon.strip('()'))
+    def get_mention(self, token):
+        return token.lstrip('@')
 
-    def add_link(self, url):
-        if not self.data.get('links'):
-            self.data['links'] = list()
+    def get_emoticon(self, token):
+        return token.strip('()')
+
+    def get_link(self, url):
         title = self.get_title(url)
-        self.data['links'].append({'url': url, "title": title})
+        return {'url': url, "title": title}
 
-    def url(self, token):
+    def check_url(self, token):
         """Check to see if a token is a url.
         """
         url_prefixes = ['http://', 'https://', 'ftp://', 'ssh://']
@@ -51,18 +57,14 @@ class Message(object):
     def get_title(self, url):
         return None
 
-    def add_email(self, email):
-        if not self.data.get('emails'):
-            self.data['emails'] = list()
-        self.data['emails'].append(email)
-
-    def email(self, token):
+    def check_email(self, token):
         if re.match(r"[^@]+@[^@]+\.[^@]+", token):
             return True
         return False
 
+
 def main():
-    test_input = '@foo hello world (allthethings) @bar https://example.com\
+     test_input = '@foo hello world (allthethings) @bar https://example.com\
                   (notbad) ftp://filez.com bye! asdf@asdf.com fake@fake,com'
 
     while True:

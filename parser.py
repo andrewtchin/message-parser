f017d01ff70479d1ee75105afcf5277018d15c66
Andrew Chin <chin@juniper.net>
2015-04-19 23:02:32 -0500

    Add method to use regex to determine token type.

--- parser.py
+++ parser.py
@@ -17,18 +17,38 @@ class Message(object):
         self.message = message
         self.data = {}
 
-    def parse(self):
-        tokens = self.message.split()
+    def parse_string(self, tokens):
         for token in tokens:
             if token.startswith('@'):
+                # re.match(r'^@\w+$', token)
                 self.add_attribute('mentions', self.get_mention(token))
             elif token.startswith('(') and token.endswith(')'):
+                # re.match(r'^\(\w+\)$', emoticon)
                 self.add_attribute('emoticons', self.get_emoticon(token))
             elif self.check_url(token):
+                # https://daringfireball.net/2010/07/improved_regex_for_matching_urls
+                # re.match(r'(?i)\b((?:[a-z][\w-]+:(?:/{1,3}|[a-z0-9%])|www\d{0,3}[.]|[a-z0-9.\-]+[.][a-z]{2,4}/)(?:[^\s()<>]+|\(([^\s()<>]+|(\([^\s()<>]+\)))*\))+(?:\(([^\s()<>]+|(\([^\s()<>]+\)))*\)|[^\s`!()\[\]{};:".,<>?]))', url)
                 self.add_attribute('links', self.get_link(token))
             elif self.check_email(token):
+                # re.match(r'[^@]+@[^@]+\.[^@]+', token)
                 self.add_attribute('emails', token)
 
+    def parse_re(self, tokens):
+        # https://rushi.wordpress.com/2008/04/14/simple-regex-for-matching-urls/
+        regex = re.compile(r'(?P<mention>^@\w+$)|(?P<emoticon>^\(\w+\)$)|(?P<url>^https?://([-\w\.]+)+(:\d+)?(/([\w/_\.]*(\?\S+)?)?)?$)')
+        for token in tokens:
+            result = regex.match(token)
+            if result:
+                if result.get('emoticon'):
+                    self.add_attribute('emoticons', self.get_emoticon(token))
+                elif result.get('mention'):
+                    self.add_attribute('mentions', self.get_mention(token))
+                elif result.get('url'):
+                    self.add_attribute('links', self.get_link(token))
+
+    def parse(self):
+        tokens = self.message.split()
+        self.parse_re(tokens)
         return json.dumps(self.data, sort_keys=True, indent=2)
 
     @ensure_key
@@ -64,7 +84,7 @@ class Message(object):
 
 
 def main():
-     test_input = '@foo hello world (allthethings) @bar https://example.com\
+    test_input = '@foo hello world (allthethings) @bar https://example.com\
                   (notbad) ftp://filez.com bye! asdf@asdf.com fake@fake,com'
 
     while True:

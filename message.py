b39ff7ae92914ec117224cdf92939019d3b7a56d
Andrew Chin <chin@juniper.net>
2015-04-21 08:52:24 -0500

    Remove regex group search. Do each search independently.

--- message.py
+++ message.py
@@ -1,6 +1,5 @@
 """This module extracts information from a message string."""
 
-from collections import defaultdict
 import json
 import re
 import urllib2
@@ -18,15 +17,27 @@ class Message(object):
         message (str): Contents of the message.
     """
     MAX_MESSAGE_LEN = 1000000
-    ATTR_REGEX = re.compile(r"^@(?P<mentions>\w+)"
-                            r"|\((?P<emoticons>\w{1,15})\)"
-                            r"|(?P<links>https?://([-\w\.]+)+(:\d+)?(/([\w/_\.]*(\?\S+)?)?)?)")
+    MENTIONS_REGEX = re.compile(r"(?:^| )@(\w+)")
+    EMOTICONS_REGEX = re.compile(r"\((\w{1,15})\)")
+    LINKS_REGEX = re.compile(r"(https?://([-\w\.]+)+(:\d+)?(/([\w/_\.]*(\?\S+)?)?)?)")
 
     def __init__(self, message=''):
         if len(message) > Message.MAX_MESSAGE_LEN:
             self.message = message[:Message.MAX_MESSAGE_LEN]
         else:
             self.message = message
+
+    def search(self, key, regex, result):
+        """Search message for regex matches and store result.
+
+        Args:
+            key (str): key to store matches.
+            regex (compiled re): regex to search for.
+            result (dict): store match list at key.
+        """
+        matches = regex.findall(self.message)
+        if matches:
+            result[key] = matches
 
     def parse(self):
         """Return desired information from the message.
@@ -36,23 +47,17 @@ class Message(object):
         Returns:
             Dict of attributes extracted from the message.
         """
-        matched_groupdicts = [match.groupdict()
-                              for match in Message.ATTR_REGEX.finditer(self.message)]
+        result = {}
+        searches = [('mentions', Message.MENTIONS_REGEX),
+                    ('emoticons', Message.EMOTICONS_REGEX),
+                    ('links', Message.LINKS_REGEX)]
 
-        result = defaultdict(set)
-        for match_dict in matched_groupdicts:
-            for key, value in match_dict.iteritems():
-                if value is not None:
-                    result[key].add(value)
-
-        # Convert sets of tokens to lists of tokens.
-        result = {key: list(value) for key, value in result.iteritems()}
+        for key, regex in searches:
+            self.search(key, regex, result)
 
         if result.get('links'):
-            enhanced_links = []
-            for link in result['links']:
-                enhanced_links.append(self.get_link(link))
-            result['links'] = enhanced_links
+            links = [self.get_link(match[0]) for match in result['links']]
+            result['links'] = links
 
         return result
 
